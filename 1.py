24ac4ac4e559f79a46a6f827d07726b435688a61
Nadya Solovyeva <nadya.night.12@gmail.com>
2022-10-06 21:41:02 +0300

    Add file

---
+++ 1.py
@@ -0,0 +1,7 @@
+name = "Надежда"
+surname = "Соловьёва"
+year_of_birth = 2004
+print (name, surname, year_of_birth, sep="_") # выводим переменные разделённые "_"
+name, surname = surname, name # меняем переменные с именем и фамилией местами
+year_of_birth1 = year_of_birth + 60 # прибавляем к дате рождения 60
+print (name, surname, year_of_birth1) # выводим переменные
